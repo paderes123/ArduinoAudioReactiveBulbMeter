6306f547ead9155f0021f6ef4209af5e817d34a7
TECHNONATICS <paderesbryan08@gmail.com>
2025-12-09 13:55:47 +0800

    Add serial port support and refresh functionality
    Added `System.IO.Ports` NuGet package to enable serial port communication. Updated the "Refresh Ports" button in `MainPage.xaml` to trigger the `RefreshPorts_Clicked` event. Implemented the `InitializeSerialPorts` method in `MainPage.xaml.cs` to populate the `SerialPortPicker` with available ports or display a message if none are available.

--- ArduinoAudioReactiveBulbMeter.App/MainPage.xaml.cs
+++ ArduinoAudioReactiveBulbMeter.App/MainPage.xaml.cs
@@ -1,10 +1,39 @@
-﻿namespace ArduinoAudioReactiveBulbMeter.App
+﻿using System.IO.Ports;
+
+namespace ArduinoAudioReactiveBulbMeter.App
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void RefreshPorts_Clicked(object sender, EventArgs e)
+        {
+            InitializeSerialPorts();
+        }
+
+        private void InitializeSerialPorts()
+        {
+            SerialPortPicker.Items.Clear();
+            var ports = SerialPort.GetPortNames();
+
+            if (ports.Length > 0)
+            {
+                foreach (var port in ports)
+                {
+                    SerialPortPicker.Items.Add(port);
+                }
+
+                SerialPortPicker.SelectedIndex = 0;
+                SerialPortPicker.IsEnabled = true;
+            }
+            else
+            {
+                SerialPortPicker.Title = "No Ports Available";
+                SerialPortPicker.IsEnabled = false;
+            }
         }
     }
 }
